48f09d46eb02ce4d56a17d68188c404301f96628
Mateusz Bryla <brylamat@gmail.com>
2019-01-21 19:49:24 +0100

    search works

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -18,14 +18,39 @@ class SearchForm extends PureComponent {
     dispatchFetchMovie: PropTypes.func.isRequired,
   };
 
-  onClick = () => {
-    this.props.dispatchFetchMovie('Batman Begins', 2005);
+  state = {
+    title: '',
+    releaseYear: '',
+  };
+
+  onTitleChange = (event) => {
+    this.setState({
+      ...this.state,
+      title: event.target.value,
+    });
+  }
+
+  onYearChange = (event) => {
+    this.setState({
+      ...this.state,
+      releaseYear: event.target.value,
+    });
+  }
+
+  onSearch = () => {
+    const { dispatchFetchMovie } = this.props;
+    const { title, releaseYear } = this.state;
+    dispatchFetchMovie(title, releaseYear);
   }
 
   render() {
     return (
       <div>
-        <button type="button" onClick={this.onClick}>search</button>
+        <label htmlFor="search-title">Title</label>
+        <input id="search-title" name="title" type="text" value={this.state.title} onChange={this.onTitleChange}/>
+        <label htmlFor="search-year">Release Year</label>
+        <input id="search-year" name="release year" type="number" min="1900" max="2019" value={this.state.releaseYear} onChange={this.onYearChange}/>
+        <button type="button" onClick={this.onSearch}>search</button>
       </div>
     );
   }
