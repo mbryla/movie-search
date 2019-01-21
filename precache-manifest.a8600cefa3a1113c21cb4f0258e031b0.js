07afe952c91d3ced416fda098f460302b048c6ce
Mateusz Bryla <brylamat@gmail.com>
2019-01-21 21:34:33 +0100

    Updates

---
+++ precache-manifest.a8600cefa3a1113c21cb4f0258e031b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05c144d15085a2fa07d3",
+    "url": "/movie-search/static/js/runtime~main.05c144d1.js"
+  },
+  {
+    "revision": "c1da1f3280f91668bbec",
+    "url": "/movie-search/static/js/main.c1da1f32.chunk.js"
+  },
+  {
+    "revision": "f9b7d556b0c6ccd464e1",
+    "url": "/movie-search/static/js/1.f9b7d556.chunk.js"
+  },
+  {
+    "revision": "c1da1f3280f91668bbec",
+    "url": "/movie-search/static/css/main.29bff4ea.chunk.css"
+  },
+  {
+    "revision": "82c90daa2e6d0b6d63b04cdb21cc0080",
+    "url": "/movie-search/index.html"
+  }
+];
